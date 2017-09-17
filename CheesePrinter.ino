31e1cde8e6686f1e225e812bb0d272f7dcd46515
Richard Johnson <cuzzle@gmail.com>
2017-09-16 20:25:52 -0700

    Changed to use i2c

--- CheesePrinter.ino
+++ CheesePrinter.ino
@@ -5,60 +5,129 @@ Author:  Tony and Richard
 */
 
 #include <Servo.h>
-#include <SPI.h>
-#include <Pixy.h>
+#include <Wire.h>
+#include <PixyI2C.h>
 
 #define spd 600 //ms between pulses
-#define mm *40 // 80 steps per millimeter
+#define xyspd 440 //ms for x or y movement
+#define slowspd 2500  //for dairyqueen style things
+#define mm *40 // 40 steps per millimeter
 #define pulseDelay 70 //ms delay
 
 Servo cheese; //servo for cheese trigger
-Pixy pixy; //load the pixi cam
+PixyI2C pixy(0x54);
+int stepsPerMM = 40;
 
 long int xpos, ypos, zpos; //variables to hold current positions
 int outputs[6] = { 2, 3, 4, 5, 6, 7 }; //Stepper ports
 int endstops[3] = { 14, 15, 16 }; //endstops on A0, A1, A2
+int buttonPin = 11;
+
+long int maxX = 575; //find and set maximum values and check whenever you move.
+long int maxY = 425; //far back
+long int maxZ = 0;
+
 void setup() {
 	Serial.begin(9600);
 	cheese.attach(8); //servo data is on pin 8
+	cheese.write(0); //check to see if this calms it down
+	pinMode(buttonPin, INPUT_PULLUP);
+
 	for (int a = 0; a < 6; a++) {
 		pinMode(outputs[a], OUTPUT); //all stepper control outputs
 		digitalWrite(outputs[a], 1);
 	}
+
 	for (int a = 0; a < 3; a++) {
 		pinMode(endstops[a], INPUT_PULLUP); //endstop inputs
 	}
 
-	//pixy.init(); //probably won't work until we connect it.
+	Wire.setClock(100000); //Try and change the speed
+
+	pixy.init(); //probably won't work until we connect it.
 
 	homePrinter();
 	movexy(5 mm, 5 mm); //pull back from the endstops
-	movez(10 mm);
+	movez(7); //cracker taking picture level
 	xpos = 0;
 	ypos = 0;
 	zpos = 0;
 }
 
 void loop() {
-
-	//getCrackers();
-	//delay(2000);
-	drawCheeseSquare(55, 55, 100, 200);
+	bool buttonState = 1;
+	buttonState = digitalRead(buttonPin);
+	if (buttonState == 0)
+	{
+		Serial.println("You pressed the button!");
+		getCrackers();
+	}
 
 }
 
-void getCrackers() { //method checks pixycam for cracker blocks and prints the location, will need to change to return the values to print
+void buttonPress()
+{
+	int buttonState = 0;
+	buttonState = digitalRead(buttonPin);
+	//Serial.print(buttonState);
+	if (buttonState == 1)
+	{
+		Serial.println("pressed button");
+		getCrackers();
+	}
+	else Serial.println("Seriosly Dude");
+
+	delay(500);
+}
+
+
+void cheeseCracker(int x, int y)
+{
+
+	//delay(8000);
+	Serial.print("Currently at: ");
+	Serial.print(xpos);
+	Serial.print(" , ");
+	Serial.println(ypos);
+	Serial.print("Apply Cheese to: ");
+	Serial.print(x);
+	Serial.print(" , ");
+	Serial.println(y);
+
+	movez(130); //move around height
+	applyCheese(x, y);
+	movez(-130); //back to starting height
+	delay(500);
+}
+
+void getCrackers() { //method checks pixycam for cracker blocks and add them to the cracker array
 	uint16_t blocks;
-	int i;
+	Serial.println("Pre Get Crackers");
 	blocks = pixy.getBlocks();
+	Serial.println("Getting Crackers");
+	Serial.println(blocks);
 	if (blocks) {
-		for (i = 0; i < blocks; i++)
+		for (int i = 0; i < blocks; i++)
 		{
-			Serial.print("Cracker found at: ");
+			Serial.print("Pixy coordinates:");
 			Serial.print(pixy.blocks[i].x);
-			Serial.print(" , ");
+			Serial.print(" = x, y = ");
 			Serial.println(pixy.blocks[i].y);
+
+			Serial.print("Pritner coordinates:");
+			Serial.print( map(pixy.blocks[i].x, 90, 255, 50, 575) );
+			Serial.print(" = x, y = ");
+			Serial.println( map(pixy.blocks[i].y, 30, 166, 125, 435) );
+
+			Serial.print("Get Crackers Currently at: ");
+			Serial.print(xpos);
+			Serial.print(" , ");
+			Serial.println(ypos);
+			cheeseCracker( map(pixy.blocks[i].x, 90, 255, 50, 575), map(pixy.blocks[i].y, 30, 166, 125, 435)); //maps pixy cam coordinates to printbed
 		}
+		moveto(0, 400 mm);
+		delay(5000);
+		home();
 	}
 }
 
@@ -73,56 +142,47 @@ void homePrinter()
 	Serial.print("Homing x...");
 	while (!digitalRead(14)) { //home x
 		pulsex(0);
-		delayMicroseconds(spd);
+		delayMicroseconds(xyspd);
 	}
 	Serial.println("Homed");
 	Serial.print("Homing y...");
 	while (!digitalRead(15)) { //home y
 		pulsey(0);
-		delayMicroseconds(spd);
+		delayMicroseconds(xyspd);
 	}
 	Serial.println("Homed");
 }
 
-void drawCheeseSquare(long int x_start, long int y_start, long int x_size, long int y_size)
-{
-	movexy(x_start mm, y_start mm);
-	movez(55 mm); //raise the printbed
-	squeezeCheese(1); //start cheesing
-	movexy(x_size mm, 0);
-	movexy(0, y_size mm);
-	movexy(-x_size mm, 0);
-	movexy(0, -y_size mm); //draw a square
-	squeezeCheese(0);
-	movez(-55 mm);
-	home(); //return back to beginnning
-	delay(500);
-}
-
 void squeezeCheese(bool on) {
 	if (on) {
-		cheese.write(120); //trigger on angle
+		cheese.write(40); //trigger on angle
 		Serial.println("Squeezing the Cheese");
 	}
 	else {
-		cheese.write(40); //trigger off angle
+		cheese.write(0); //trigger off angle
 		Serial.println("No more Cheese Please");
 	}
 	delay(150);  //servo move delay need to test this
 }
 
+void moveto(long int x, long int y)
+{
+	Serial.print("Currently at: ");
+	Serial.print(xpos);
+	Serial.print(" , ");
+	Serial.println(ypos);
+	Serial.print("Moving to: ");
+	Serial.print(x);
+	Serial.print(" , ");
+	Serial.println(y);
+	movexy(x - xpos, y - ypos);
+}
+
+
 void movexy(long int x, long int y) { //moves to a new x and y position
 	Serial.println("Moving XY");
-	Serial.print("x: ");
-	Serial.print(x);
-	Serial.print(" y: ");
-	Serial.println(y);
 	xpos += x;
 	ypos += y;
-	//Serial.print("x = ");
-	//Serial.println(xpos);
-	// Serial.print("y = ");
-	//Serial.println(ypos);
 	bool xdir = x > 0;
 	bool ydir = y > 0;
 	x = abs(x);
@@ -146,7 +206,7 @@ void movexy(long int x, long int y) { //moves to a new x and y position
 	float ycount = 0;
 	long int xset = 0;
 	long int yset = 0;
-	while (yset != y || xset != x) { //haven't arrived yet
+	while (yset != y || xset != x) { 
 		xcount++;
 		ycount++;
 		if (xcount >= xstep && xset != x) {
@@ -155,7 +215,7 @@ void movexy(long int x, long int y) { //moves to a new x and y position
 			//Serial.println(xset);
 			xcount -= xstep;
 			pulsex(xdir);
-			delayMicroseconds(spd);
+			delayMicroseconds(xyspd);
 		}
 		if (ycount >= ystep && yset != y) {
 			yset++;
@@ -163,13 +223,14 @@ void movexy(long int x, long int y) { //moves to a new x and y position
 			// Serial.println(yset);
 			ycount -= ystep;
 			pulsey(ydir);
-			delayMicroseconds(spd);
+			delayMicroseconds(xyspd);
 		}
 	}
 }
 
 void movez(long int z) {
 	Serial.println("Moving Z");
+	z *= stepsPerMM;
 	zpos += z;
 	bool zdir = z > 1;
 	for (int a = 0; a < abs(z); a++) {
@@ -178,7 +239,19 @@ void movez(long int z) {
 	}
 }
 
+void movezslow(long int z) {
+	z *= stepsPerMM;
+	Serial.println("Moving Z");
+	zpos += z;
+	bool zdir = z > 1;
+	for (int a = 0; a < abs(z); a++) {
+		pulsez(zdir);
+		delayMicroseconds(slowspd);
+	}
+}
+
 void home() {
+	//Serial.print
 	movez(-zpos);
 	movexy(-xpos, -ypos);
 }
@@ -190,7 +263,7 @@ void pulsex(bool dir) {
 	digitalWrite(2, 1);
 }
 void pulsey(bool dir) {
-	digitalWrite(5, !dir);
+	digitalWrite(5, dir);
 	digitalWrite(4, 0);
 	delayMicroseconds(pulseDelay);
 	digitalWrite(4, 1);
@@ -204,9 +277,19 @@ void pulsez(bool dir) {
 
 void applyCheese(long int x, long int y)
 {
-	//TODO: make sure it's at the right location
-	movexy(x, y);
+	Serial.print("Currently at: ");
+	Serial.print(xpos);
+	Serial.print(" , ");
+	Serial.println(ypos);
+	Serial.print("Moving to: ");
+	Serial.print(x);
+	Serial.print(" , ");
+	Serial.println(y);
+
+	moveto(x mm, y mm);
+	movez(70);
 	squeezeCheese(1);
-	movez(10); //move the nozzle up while dispensing cheese
+	movezslow(-20); //move the nozzle up while dispensing cheese
 	squeezeCheese(0);
+	movez(-50);
 }
